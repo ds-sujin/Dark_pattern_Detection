fa1644352adb63ede0c177cc50d10f5ef225756a
정수진 <124863799+ds-sujin@users.noreply.github.com>
2025-04-09 05:34:31 +0900

    메뉴 리스트, 선택, 타이머, 사용자 이름 완료

--- kiosk/client/src/App.js
+++ kiosk/client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
+import MenuListPage from './pages/MenuListPage';
 
 function App() {
   return (
-    <div className="App">
-      <StartPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/menu" element={<MenuListPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
