16288add80bd18e6e8490f54475a708932f83093
소영 <soyoung--@naver.com>
2025-04-11 03:45:55 +0900

    시간 초과 팝업 완성

--- kiosk/client/src/App.js
+++ kiosk/client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import MenuListPage from './pages/MenuListPage';
 import CompletePage from './pages/CompletePage';
-import TimerPage from './pages/TimerPage'; // 상단에 import 추가
+
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" element={<StartPage />} />
         <Route path="/menu" element={<MenuListPage />} />
         <Route path="/complete" element={<CompletePage />} />
-        <Route path="/timer" element={<TimerPage />} /> {/* ✅ 테스트용 경로 */}
+        
       </Routes>
     </BrowserRouter>
   );
