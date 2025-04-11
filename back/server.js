64d327b4131920c02be54385288da113b924df1e
정수진 <124863799+ds-sujin@users.noreply.github.com>
2025-04-11 19:27:48 +0900

    프론트,백엔드 기본 세팅, 메인화면 설정

--- back/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app");
-
-// 서버 포트 설정
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
