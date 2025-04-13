529ba6860063b368877e31350bb2b701253cf253
정수진 <124863799+ds-sujin@users.noreply.github.com>
2025-04-13 18:33:46 +0900

    90도 돌리고, 화면 비율 맞춰서 채우기

--- kiosk_90/client/src/App.js
+++ kiosk_90/client/src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import MenuListPage from './pages/MenuListPage';
-import CompletePage from './pages/CompletePage';
+//import CompletePage from './pages/CompletePage';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/menu" element={<MenuListPage />} />
-        <Route path="/complete" element={<CompletePage />} />
-        
-      </Routes>
-    </BrowserRouter>
+    <div className="w-screen h-screen overflow-visible">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/menu" element={<MenuListPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
