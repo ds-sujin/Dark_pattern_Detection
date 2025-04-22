6930c639980eab4127d13a00daf0eb6cb789a5be
정수진 <124863799+ds-sujin@users.noreply.github.com>
2025-04-22 19:50:58 +0900

    로그인 화면 구현 완료

---
+++ web/client/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // 스타일 분리
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">DARKPATTERN DETECTION</Link>
+      </div>
+      <ul className="navbar-menu">
+        <li><Link to="/analyze">다크패턴 분석하기</Link></li>
+        <li><Link to="/history">나의 분석기록</Link></li>
+        <li><Link to="/notice">공지사항</Link></li>
+        <li><Link to="/mypage">마이페이지</Link></li>
+      </ul>
+      <div className="navbar-login">
+        <Link to="/login" className="login-btn">로그인/회원가입</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
