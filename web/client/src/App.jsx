8dbca42e3b705cef110d18b2eb1d28101d9042e8
jane-datascience <jane08170@naver.com>
2025-05-08 02:19:00 +0900

    mongodb와 연결중중

--- web/client/src/App.jsx
+++ web/client/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
-
+import RegisterPage from './pages/RegisterPage';
+import RegisterCompletePage from './pages/RegisterCompletePage'; 
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route path="/notice" element={<div>공지사항 페이지</div>} />
         <Route path="/mypage" element={<div>마이페이지</div>} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/register-complete" element={<RegisterCompletePage />} />
       </Routes>
     </Router>
   );
