dc56936b6e759d3d00d763efd28339d2dca05e14
yubin <dbqls8969@naver.com>
2025-05-18 03:42:24 +0900

    ✨ upload 기능 다시 올리기

---
+++ web/server/db/image.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema({
+  url: String,
+  user_id: String,
+  user_name: String,
+}, {
+  versionKey: false // __v 필드 아예 생성하지 않음
+});
+
+module.exports = mongoose.model('Image', imageSchema, 'image');
