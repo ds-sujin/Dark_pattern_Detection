7676f7876118e1d79e87a5215223b4788a5385ca
yubin <dbqls8969@naver.com>
2025-05-27 22:28:06 +0900

    upload 재수정

--- web/server/drive/googleDrive.js
+++ web/server/drive/googleDrive.js
@@ -1,34 +1,50 @@
 const { google } = require('googleapis');
 const fs = require('fs');
-const path = require('path');
+require('dotenv').config();
 
-// ✅ JSON 키파일을 통한 인증 방식
-const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, '../restored-key.json'),
-  scopes: ['https://www.googleapis.com/auth/drive'],
-});
+const auth = new google.auth.JWT(
+  process.env.GOOGLE_CLIENT_EMAIL,
+  null,
+  process.env.GOOGLE_DRIVE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  ['https://www.googleapis.com/auth/drive']
+);
+
+const drive = google.drive({ version: 'v3', auth });
 
 async function uploadToDrive(filePath, fileName) {
-  const authClient = await auth.getClient();
-  const drive = google.drive({ version: 'v3', auth: authClient });
-
   const fileMetadata = {
     name: fileName,
     parents: [process.env.GOOGLE_DRIVE_FOLDER_ID],
   };
 
   const media = {
-    mimeType: 'image/png',
+    mimeType: 'image/jpeg',
     body: fs.createReadStream(filePath),
   };
 
-  const file = await drive.files.create({
+  const response = await drive.files.create({
     resource: fileMetadata,
     media,
-    fields: 'id, webViewLink',
+    fields: 'id',
   });
 
-  return file.data.webViewLink;
+  const fileId = response.data.id;
+
+  // 파일 공개 설정
+  await drive.permissions.create({
+    fileId,
+    requestBody: {
+      role: 'reader',
+      type: 'anyone',
+    },
+  });
+
+  const result = await drive.files.get({
+    fileId,
+    fields: 'webViewLink, webContentLink',
+  });
+
+  return result.data.webViewLink;
 }
 
 module.exports = { uploadToDrive };
