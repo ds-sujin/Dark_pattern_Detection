872c9e034e52cf6d21cc6ae78b6b7d96b93a17f4
정수진 <124863799+ds-sujin@users.noreply.github.com>
2025-06-09 00:57:40 +0900

    폴더 구조 정리 web/client

--- web/client/src/App.jsx
+++ web/client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
+import NewsPage from './pages/NewsPage'; // ✅ 추가
+import AboutDark from './pages/AboutDark'; // ✅ 추가
 
 
 function App() {
@@ -9,8 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/analyze" element={<div>다크패턴 분석하기 페이지</div>} />
-        <Route path="/learn" element={<div>다크패턴 알아보기 페이지</div>} />
-        <Route path="/news" element={<div>관련 뉴스</div>} />
+        <Route path="/learn" element={<AboutDark/>} />
+        <Route path="/news" element={<NewsPage />} />
         <Route path="/history" element={<div>나의 분석기록</div>} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
