3be297b73aee5dd8b2f92a9383204755fc157ccc
정수진 <124863799+ds-sujin@users.noreply.github.com>
2025-06-13 00:13:21 +0900

    app.py mongoDB에 filename도 같이 저장

--- web/model_server/app.py
+++ web/model_server/app.py
@@ -22,14 +22,20 @@ def predict():
         return jsonify({"error": "filename 누락됨"}), 400
 
     # ✅ server/input_image에서 이미지 로드
-    img_path = os.path.join("input_image", filename)  # ✅ 변경
+    img_path = os.path.join("input_image", filename)
     if not os.path.exists(img_path):
         return jsonify({"error": f"{img_path} 경로에 이미지가 존재하지 않습니다."}), 404
 
     try:
         prediction_results = process_image_and_predict(img_path)
+
+        # ✅ 예측 결과에 filename을 추가
+        for result in prediction_results:
+            result["filename"] = filename
+
         if prediction_results:
             predicate_col.insert_many(prediction_results)
+
         return jsonify({"message": "✅ 예측 및 저장 완료", "count": len(prediction_results)})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
